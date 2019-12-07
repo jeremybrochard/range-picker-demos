0dab92694fc598305bb06cb8e551c89b8f7e96e4
Jeremy BROCHARD <jrmy.brochard@gmail.com>
2019-12-07 17:11:45 +0100

    feat: implement DeltaNumberDirective

--- src/app/shared/delta-number.directive.ts
+++ src/app/shared/delta-number.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, AfterViewInit, ElementRef, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { UtilsService } from './utils.service';
 
 @Directive({
   selector: '[appDeltaNumber]'
 })
 export class DeltaNumberDirective implements OnChanges {
 
-  @Input() input: any;
+  @Input() input: Date;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private utilsService: UtilsService) { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.input) {
-      // Todo: replace date selection by floating number
-      // Use utils service
+      const floatingValue = this.utilsService.fromDateToFloating(this.input);
+      const displayValue = floatingValue >= 0 ? `D+${floatingValue}` : `D${floatingValue}`;
+      this.el.nativeElement.value = displayValue;
     }
   }
 
