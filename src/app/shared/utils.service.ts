eae3fd2414c512e354115cf0d260f1ccd7e3df02
Jeremy BROCHARD <jrmy.brochard@gmail.com>
2019-12-07 17:11:24 +0100

    feat: implement functions to convert floating and fixed date models

--- src/app/shared/utils.service.ts
+++ src/app/shared/utils.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FloatingRange } from 'src/app/model/floating-range.model';
 import { DateRange } from 'src/app/model/date-range.model';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,37 @@ export class UtilsService {
 
   constructor() { }
 
-  fromDateRangeToFloatingRange(date: DateRange): FloatingRange {
-    // Todo: convert using moment js
-    return new FloatingRange();
+  fromDateToFloating(date: Date): number {
+    const dateInMoment = moment(date);
+    const now = moment();
+
+    return dateInMoment.diff(now, 'days');
   }
 
-  fromFloatingRangeToDateRange(date: FloatingRange): DateRange {
-    // Todo: convert using moment js
-    return new DateRange();
+  fromFloatingToDate(floatingNumber: number): Date {
+    const now = moment();
+
+    return now.add(floatingNumber, 'days').toDate();
+  }
+
+  fromDateRangeToFloatingRange(date: DateRange): FloatingRange | null {
+    if (date) {
+      const fromDelta = this.fromDateToFloating(date.from);
+      const toDelta = this.fromDateToFloating(date.to);
+
+      return new FloatingRange({ from: fromDelta, to: toDelta });
+    }
+    return null;
+  }
+
+  fromFloatingRangeToDateRange(date: FloatingRange): DateRange | null {
+    if (date) {
+      const fromDate = this.fromFloatingToDate(date.from);
+      const toDate = this.fromFloatingToDate(date.to);
+
+      return new DateRange({ from: fromDate, to: toDate });
+    }
+    return null;
   }
 
 }
